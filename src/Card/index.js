8bd9bdca64987e1f2e2a45d8a6a9b107b56e2e01
Eric (Yiqi) Hou <erichou2004@gmail.com>
2017-08-12 19:43:05 -0700

    Styling changes w/ cards and rows

--- src/Card/index.js
+++ src/Card/index.js
@@ -1,4 +1,6 @@
 import {Card} from './Card';
+import {CardImageArea} from './CardImageArea';
+import {CardTextArea} from './CardTextArea';
 
 export default Card;
-export {Card};
+export {Card, CardImageArea, CardTextArea};
